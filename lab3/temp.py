0b11f601f27c82f6dbe730ecef7fb3ebf95140f5
sawertynn <sawertynn@gmail.com>
2025-04-02 15:00:17 +0200

    lab3: actual start

--- lab3/temp.py
+++ lab3/temp.py
@@ -5,6 +5,10 @@ Author: Katarzyna Nałęcz-Charkiewicz
 from board import Board
 from player import Player
 
+import random
+
+MAX_VALUE = 999
+
 
 class MinMaxPlayer(Player):
     def __init__(self, name: str, depth_limit: int):
@@ -12,13 +16,24 @@ class MinMaxPlayer(Player):
         self.depth_limit = depth_limit
 
     def make_move(self, board: Board, your_side: str):
+        empty_indexes = board.empty_indexes()
+        return empty_indexes[random.randrange(len(empty_indexes))]
+
+
         # TODO
         return None
 
     def minimax(self, board: Board, side: str, depth: int):
-        # TODO
+        if depth == 0 or board.who_is_winner() is not None:
+            return None, self.evaluate(board, side)
+        
+        for move in board.empty_indexes():
+            new_board = board.clone()
+            new_board.register_move(move)
+            
         return None, None
 
     def evaluate(self, board: Board, side: str):
-        # TODO
+        if winner := board.who_is_winner():
+            return MAX_VALUE if winner == side else -1 * MAX_VALUE
         return 0
